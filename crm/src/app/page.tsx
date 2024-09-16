5b8950c0b59854058d8af69eb5332dffadef4073
Mykhailo Zykov <mykhailozykov@gmail.com>
2024-09-16 13:21:09 +0300

    dashboard page init

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,11 +1,12 @@
 import AddCompanyButton from './components/add-company-button';
-import StatusLabel, { Status } from './components/status-label';
+
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl m-10">Home page </h1>
       <AddCompanyButton/>
+     
     </main>
   );
 }
