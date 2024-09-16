18fc644ce8d69c422ff927ce6e43af2360394470
Mykhailo Zykov <mykhailozykov@gmail.com>
2024-09-16 10:50:41 +0300

    button component added

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -3,7 +3,7 @@ import StatusLabel, { Status } from './components/status-label';
 export default function Home() {
   return (
     <main>
-      <h1 className="text-xl m-10">Home page</h1>
+      <h1 className="text-xl m-10">Home page {new Date().toTimeString()}</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
